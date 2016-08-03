85a7cde82e3d987c29ec17dd0421cf6981f4ee87
sean lon <seanlon.loh@bertelsmann.de>
2016-08-03 21:50:42 +0800

    fix build

--- app/containers/VisitorLogin/index.js
+++ app/containers/VisitorLogin/index.js
@@ -19,54 +19,57 @@ export default class VisitorLogin extends React.Component { // eslint-disable-li
   render() {
     return (
    
-      <div>
-         <form> 
-    <div className={materials.formgroup}>
-      <select>
-        <option>Interview</option>
-        <option>Visitation</option>
-        <option>Deliver / Receive Item</option>
-        <option>Maintenance Service </option>
-      </select>
-      <label htmlFor="select" className={materials.controllabel}><FormattedMessage {...messages.purpose} /></label><i className={materials.bar}></i>
-    </div>
-    <div className={materials.formgroup}>
-      <input type="text" required="required"/>
-      <label htmlFor="input" className={materials.controllabel}><FormattedMessage {...messages.name} /></label><i className={materials.bar}></i>
-    </div>
-    <div className={materials.formgroup}>
-      <input type="text" required="required"/>
-      <label htmlFor="input" className={materials.controllabel}><FormattedMessage {...messages.userno} /></label><i className={materials.bar}></i>
-    </div>
-    <div className={materials.formgroup}>
-      <textarea required="required"></textarea>
-      <label htmlFor="textarea" className={materials.controllabel}><FormattedMessage {...messages.summary} /></label><i className={materials.bar}></i>
-    </div>
-   
-    <div className={materials.checkbox}>
-      <label>
-        <input type="checkbox"/><i className={materials.helper}></i><FormattedMessage {...messages.tnc} />
-      </label>
-    </div>
- 
- 
-  </form>
-
-       <Button {...messages.header}  handleRoute={this.openHomePage } >
-             <FormattedMessage {...messages.submit} />
-        </Button>
- 
-        <div className={materials.middle}>
-          <h1 >  <FormattedMessage {...messages.agent} /> </h1> 
-            <Img className={materials.logo} src={AgentImgSrc} alt="agent face" /> 
-          <h3 >  <FormattedMessage {...messages.agentcontent} /> </h3> 
-
-           <Button   handleRoute={this.openHomePage } >
-                 <FormattedMessage {...messages.ok} />
-            </Button>
-     
+     <div>
+    <form>
+        <div className={materials.formgroup}>
+            <select>
+                <option>Interview</option>
+                <option>Visitation</option>
+                <option>Deliver / Receive Item</option>
+                <option>Maintenance Service </option>
+            </select>
+            <label htmlFor={"select"} className={materials.controllabel}>
+                <FormattedMessage {...messages.purpose} />
+            </label><i className={materials.bar}></i>
         </div>
-      </div>
+        <div className={materials.formgroup}>
+            <input type="text" required="required" />
+            <label htmlFor={"input"} className={materials.controllabel}>
+                <FormattedMessage {...messages.name} />
+            </label><i className={materials.bar}></i>
+        </div>
+        <div className={materials.formgroup}>
+            <input type="text" required="required" />
+            <label htmlFor={"input"} className={materials.controllabel}>
+                <FormattedMessage {...messages.userno} />
+            </label><i className={materials.bar}></i>
+        </div>
+        <div className={materials.formgroup}>
+            <textarea required="required"></textarea>
+            <label htmlFor={"textarea"} className={materials.controllabel}>
+                <FormattedMessage {...messages.summary} />
+            </label><i className={materials.bar}></i>
+        </div>
+        <div className={materials.checkbox}>
+            <label htmlFor={"checkbox"}>
+                <input type="checkbox" /><i className={materials.helper}></i>
+                <FormattedMessage {...messages.tnc} />
+            </label>
+        </div>
+    </form>
+    <Button {...messages.header} handleRoute={this.openHomePage }>
+        <FormattedMessage {...messages.submit} />
+    </Button>
+    <div className={materials.middle}>
+        <h1>  <FormattedMessage {...messages.agent} /> </h1>
+        <Img className={materials.logo} src={AgentImgSrc} alt="agent face" />
+        <h3>  <FormattedMessage {...messages.agentcontent} /> </h3>
+        <Button handleRoute={this.openHomePage }>
+            <FormattedMessage {...messages.ok} />
+        </Button>
+    </div>
+</div>
+
     );
   }
 }
